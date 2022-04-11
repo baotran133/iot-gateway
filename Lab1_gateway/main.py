173cf93f8cea164236d6166d49dd5f15fa155b47
Tran Bao <bao.tran133@hcmut.edu.vn>
2022-04-11 09:07:21 +0700

    add widget + connect hardware

--- Lab1_gateway/main.py
+++ Lab1_gateway/main.py
@@ -4,7 +4,43 @@ import paho.mqtt.client as mqttclient
 import time
 import json
 import geocoder         #Geocoder libary
+import serial.tools.list_ports
 
+mess = ""
+bbc_port="COM5"
+if len(bbc_port)>0:
+    ser=serial.Serial(port=bbc_port,baudrate=115200)
+def converse(name):
+    swich={
+        'HUMI':"humidity",
+        'TEMP':"temperature",
+        'LIGHT':"light"
+    }
+    return swich.get(name)
+
+def processData(data):
+    data = data.replace("!", "")
+    data = data.replace("#", "")
+    splitData = data.split(":")
+    print(splitData)
+    #TODO
+    id,name,value=splitData
+    name=converse(name)
+    collect_data={name:int(value)}
+    client.publish('v1/devices/me/telemetry', json.dumps(collect_data), 1)
+def readSerial():
+    bytesToread = ser.inWaiting()
+    if (bytesToread>0):
+        global mess
+        mess = mess + ser.read(bytesToread).decode("UTF-8")
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(mess[start:end + 1])
+            if (end==len(mess)):
+                mess = ""
+            else:
+                mess = mess[end+1:]
 
 BROKER_ADDRESS = "demo.thingsboard.io"
 PORT = 1883
@@ -18,14 +54,23 @@ def subscribed(client, userdata, mid, granted_qos):
 def recv_message(client, userdata, message):
     print("Received: ", message.payload.decode("utf-8"))
     temp_data = {'value': True}
+    cmd=""
     try:
         jsonobj = json.loads(message.payload)
-        if jsonobj['method'] == "setValue":
+        if jsonobj['method'] == "setLED":
             temp_data['value'] = jsonobj['params']
-            client.publish('v1/devices/me/attributes', json.dumps(temp_data), 1)
+            client.publish('v1/devices/me/LED', json.dumps(temp_data), 1)
+            cmd += "!4:LED:" + str(temp_data['value'])
+        if jsonobj['method'] == "setPump":
+            temp_data['value'] = jsonobj['params']
+            client.publish('v1/devices/me/PUMP', json.dumps(temp_data), 1)
+            cmd += "!5:PUMP:" + str(temp_data['value'])
     except:
         pass
 
+    if len(bbc_port)>0:
+        ser.write((str(cmd) + "#").encode())
+        print("msg control has been sent:" + cmd)
 
 def connected(client, usedata, flags, rc):
     if rc == 0:
@@ -37,8 +82,6 @@ def connected(client, usedata, flags, rc):
 def getLocation():
     # retrieve my own ip address
     g = geocoder.ip('me')
-    # Check coordinate
-    print("Dynamic update the coordinate in format [latitude, longitude]: " + str(g.latlng))
     # Optain the latitude, longitude
     return g.latlng
 
@@ -59,11 +102,14 @@ light_intesity = 100
 counter = 0
 while True:
     #Dynamic update the coordinate
-    latitude, longitude = getLocation()
-    collect_data = {'temperature': temp, 'humidity': humi, 'light': light_intesity,
-                    'longitude': longitude,  'latitude': latitude}
-    temp += 1
-    humi += 1
-    light_intesity += 1
-    client.publish('v1/devices/me/telemetry', json.dumps(collect_data), 1)
-    time.sleep(10)
+    # latitude, longitude = getLocation()
+    # collect_data = {'temperature': temp, 'humidity': humi, 'light': light_intesity,
+    #                 'longitude': longitude,  'latitude': latitude}
+    # temp += 1
+    # humi += 1
+    # light_intesity += 1
+    # client.publish('v1/devices/me/telemetry', json.dumps(collect_data), 1)
+    # time.sleep(10)
+    if (len(bbc_port) > 0):
+        readSerial()
+    time.sleep(1)
